05ed4f89262c2336acd43dd6900707cc500dab4d
Tara Sassel <tara.sassel@gmail.com>
2025-05-13 01:08:37 +0100

    added sidebar

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from energy_prediction import predict_energy, create_prediction_plot
 from renewables_plot import create_renewables_plot
 
 
+with st.sidebar:
+    st.title("About")
+    st.text("- Select a country from the drop down menu. \n" \
+    "- Available countries are colored in light green. \n" \
+    "- The current selection is colored in dark green \n")
+    st.write("Created in :streamlit: by Tara Sassel")
+
 st.title("Energy Spot Price Prediction")
 
 selected_country_df, europe = select_country()
 create_country_selection_plot(europe)
-st.text("- Select a country from the drop down menu. \n" \
-"- Available countries are colored in light green. \n" \
-"- The current selection is colored in dark green \n")
 
 combined_predictions, renewables = predict_energy(selected_country_df)
 create_prediction_plot(combined_predictions, selected_country_df)
